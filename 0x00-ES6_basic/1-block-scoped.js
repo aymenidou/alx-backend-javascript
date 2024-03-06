5a4892ccd06e6a839117621048c2cb405a9e57fd
Idouahman Aymen <aymanidou@gmail.com>
2024-03-06 16:11:59 +0100

    1-block-scoped.js

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+  task = false;
+  task2 = true;
+
+  return [task, task2];
+}
