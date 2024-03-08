8daa37877ca62c0b8569a869e55b15bee88cde76
Idouahman Aymen <aymanidou@gmail.com>
2024-03-08 17:33:48 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({
-    status: 200,
-    body: 'Success',
-  })).catch(() => new Error())
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
