e6b4b9a811f1bbadc5c3def91dc65a3e7e0f227e
Aymen Idouahman <aymanidou@gmail.com>
2024-03-11 16:50:14 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const res = await Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return (
-    res.map((obj) => ({
-      status: obj.status,
-      value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
-    }))
-  );
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((obj) => ({
+        status: obj.status,
+        value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+      }))
+    ));
 }
