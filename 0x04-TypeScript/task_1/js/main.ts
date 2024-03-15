821799735047a0a8edf760951c3b3e17738d1d99
Idouahman Aymen <aymanidou@gmail.com>
2024-03-15 14:22:55 +0000

    Extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,17 +1,11 @@
-interface Teacher {
-  firstName: string;
-  lastName: string;
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
   fullTimeEmployee: boolean;
   location: string;
   yearsOfExperience?: number;
   [key: string]: any; // for additional dynamic attributes
 }
-
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-console.log(teacher3);
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
