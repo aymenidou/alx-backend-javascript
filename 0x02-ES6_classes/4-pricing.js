762de24fb9343ec041f14529e10857109e56a7e8
Idouahman Aymen <aymanidou@gmail.com>
2024-03-22 14:37:29 +0000

    4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(value) {
+    if (!(this.currency instanceof Currency)) {
+      throw new TypeError('currency must be an object of Currency');
+    }
+    this._currency = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
