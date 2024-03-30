0ec4c8adcee660c47b1625b87469840704d28557
Aymen Idouahman <aymanidou@gmail.com>
2024-03-30 18:27:34 +0000

    10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,33 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  set brand(val) {
+    this._brand = val;
+  }
+
+  set motor(val) {
+    this._motor = val;
+  }
+
+  set color(val) {
+    this._color = val;
+  }
+
+  get brand() { return this._brand; }
+
+  get motor() { return this._motor; }
+
+  get color() { return this._color; }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+  cloneCar() {
+    const species = this.constructor[Symbol.species];
+    return new species();
+  }
+}
