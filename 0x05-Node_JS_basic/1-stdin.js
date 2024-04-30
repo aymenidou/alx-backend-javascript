4606051701049e59657e822ca592b2e7b1295d6e
Aymen Idouahman <aymanidou@gmail.com>
2024-04-30 23:58:00 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-process.stdin.resume();
+process.stdin.on('readable', () => {
+  const inputData = process.stdin.read();
 
-let inputData = '';
-
-process.stdin.on('data', (data) => {
-  inputData += data;
+  if (inputData) {
+    process.stdout.write(`Your name is: ${inputData}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  const name = inputData.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
   process.stdout.write('This important software is now closing\n');
 });
