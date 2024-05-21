cb11ac0f76c6ebef410e9fcca643a648660ffbd5
Aymen Idouahman <aymanidou@gmail.com>
2024-05-21 23:35:07 +0100

    13. Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+export default function createIteratorObject(report) {
+  const allEmployees = [];
+  for (const department of Object.values(report.allEmployees)) {
+    allEmployees.push(...department);
+  }
+
+  return allEmployees[Symbol.iterator]();
+}
