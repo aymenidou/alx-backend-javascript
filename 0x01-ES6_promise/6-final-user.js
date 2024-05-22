ea55c0a71e3305429af8848ad147e17133649b7e
Aymen Idouahman <aymanidou@gmail.com>
2024-05-22 10:08:45 +0100

    6. Handle multiple promises : fix lint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
+    .then((res) => {
       res.map((obj) => ({
         status: obj.status,
         value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
-      }))
-    ));
+      }));
+    });
 }
